e4c08920a13e747c224cd3bcd6ada0d5ae0ef4a2
AdamAffan <adam.affan@ucalgary.ca>
2023-03-06 07:29:20 +0000

    Step 8

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,14 +1,20 @@
 package basicjava;
 
+import java.util.Arrays;
+
 public class CCArrays {
 
-	public static void replace(char[] chars, char c, char d) {
-		// TODO Auto-generated method stub
-		
+	public static void replace(char[] charArray, char toReplace, char replaceWith) {
+		for (int index = 0; index < charArray.length; index++) {
+            if (Character.toLowerCase(charArray[index]) == Character.toLowerCase(toReplace)) {
+                charArray[index] = replaceWith;
+            }
+        }
 	}
 
-	public static void sortAlphabetic(String[] strs) {
-		// TODO Auto-generated method stub
+
+	public static void sortAlphabetic(String[] strArray) {
+		Arrays.sort(strArray, String.CASE_INSENSITIVE_ORDER);
 		
 	}
 	
