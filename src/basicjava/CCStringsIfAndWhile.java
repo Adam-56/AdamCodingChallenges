e40881b0bc556ca2effd1f5b464975b5c8305b6b
AdamAffan <adam.affan@ucalgary.ca>
2023-04-07 18:07:45 -0600

    passed tests

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -4,17 +4,45 @@ public class CCStringsIfAndWhile {
 
 	public static boolean isDigit(char c) {
 		// TODO Auto-generated method stub
-		return false;
+		return c >= '0' && c <= '9';
 	}
 
 	public static int count(String string, String string2) {
 		// TODO Auto-generated method stub
-		return 0;
-	}
+		int count = 0;
+	    String stringLower = string.toLowerCase();
+	    String charsLower = string2.toLowerCase();
 
+	    for (int i = 0; i < stringLower.length(); i++) {
+	        if (charsLower.indexOf(stringLower.charAt(i)) != -1) {
+	            count++;
+	        }
+	    }
+	    return count;
+	}
+	
 	public static int smallestDigit(int i) {
 		// TODO Auto-generated method stub
-		return 0;
+		if (i == 0) {
+	        return 0;
+	    }
+
+	    i = abs(i);
+	    int smallest = 9;
+
+	    while (i > 0) {
+	        int digit = i % 10;
+	        if (digit < smallest) {
+	            smallest = digit;
+	        }
+	        i /= 10;
+	    }
+
+	    return smallest;
 	}
 
+	private static int abs(int i) {
+		// TODO Auto-generated method stub
+		return i < 0 ? -i : i;
+    }
 }
